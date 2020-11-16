355dcb2e4abd00824cb36d9a5fe5fdb8a9a0bede
Samuel Koskelainen <samuelkoskelainen@gmail.com>
2020-11-16 23:54:52 +0200

    removed app.get and left the rest of the method

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 app.use(express.static(__dirname + "/client/build"))
+  ('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
+  })
+
 
 const server = require('http').createServer(app)
 
