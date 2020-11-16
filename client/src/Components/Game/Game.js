100f46f2e280cdac226bd6d2fd79608e34a5ce32
Samuel Koskelainen <samuelkoskelainen@gmail.com>
2020-11-17 01:55:07 +0200

    changed client link to wss://agile-thundsdf.herokuapp.com

--- client/src/Components/Game/Game.js
+++ client/src/Components/Game/Game.js
@@ -3,7 +3,7 @@ import { w3cwebsocket as W3CWebSocket } from 'websocket'
 import { Link } from 'react-router-dom'
 import './Game.css'
 
-const socket = new W3CWebSocket(`wss://https://agile-tundra-77439.herokuapp.com/`)
+const socket = new W3CWebSocket(`wss://agile-tundra-77439.herokuapp.com`)
 const Game = (props) => {
   const [clientId, setClientId] = useState('')
   const [counter, setCounter] = useState(0)
