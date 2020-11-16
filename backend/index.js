e19763495228189bea58280696d917a06d0755a8
Samuel Koskelainen <samuelkoskelainen@gmail.com>
2020-11-17 00:19:58 +0200

    changed backend and added homepage again

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-app.use(express.static(__dirname + "/client/build"))
-app.use('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
+const root = path.join(__dirname, 'client', 'build')
+app.use(express.static(root))
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root })
 })
 
 
