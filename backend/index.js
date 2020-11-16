7af8c554f77193d394f864d70e7462c9b6ba0d68
Samuel Koskelainen <samuelkoskelainen@gmail.com>
2020-11-17 00:24:54 +0200

    changed back

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const root = path.join(__dirname, 'client', 'build')
-app.use(express.static(root))
+app.use(express.static(__dirname + "/client/build"))
 app.get('*', (req, res) => {
-  res.sendFile('index.html', { root })
+  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
 })
 
 
