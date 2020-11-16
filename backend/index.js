9b7c00f40af25da7f707bf40bf666cdbad1ec14d
Samuel Koskelainen <samuelkoskelainen@gmail.com>
2020-11-16 23:48:57 +0200

    removed app.get('*') method from backend

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 app.use(express.static(__dirname + "/client/build"))
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html"))
-})
-
 
 const server = require('http').createServer(app)
 
